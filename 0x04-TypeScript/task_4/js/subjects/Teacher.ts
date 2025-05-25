c477e61ffaf6387e6c9be510cbeca7b4aa6aa18a
Meron Solomon <meronsolomondana@gmail.com>
2025-05-25 23:48:14 +0300

    update task_4

--- 0x04-TypeScript/task_4/js/subjects/Teacher.ts
+++ 0x04-TypeScript/task_4/js/subjects/Teacher.ts
@@ -1,6 +1,35 @@
-namespace Subjects {
-  export interface Teacher {
-    firstName: string;
-    lastName: string;
-  }
-}
+/// <reference path="./subjects/Cpp.ts" />
+/// <reference path="./subjects/Java.ts" />
+/// <reference path="./subjects/React.ts" />
+
+import { Subjects } from "./subjects/Subject";
+
+// Export constants for each subject
+export const cpp = new Subjects.Cpp();
+export const java = new Subjects.Java();
+export const react = new Subjects.React();
+
+// Export teacher object
+export const cTeacher: Subjects.Teacher = {
+  firstName: "John",
+  lastName: "Doe",
+  experienceTeachingC: 10,
+};
+
+// Cpp
+console.log("C++");
+cpp.setTeacher(cTeacher);
+console.log(cpp.getRequirements());
+console.log(cpp.getAvailableTeacher());
+
+// Java
+console.log("Java");
+java.setTeacher(cTeacher);
+console.log(java.getRequirements());
+console.log(java.getAvailableTeacher());
+
+// React
+console.log("React");
+react.setTeacher(cTeacher);
+console.log(react.getRequirements());
+console.log(react.getAvailableTeacher());
